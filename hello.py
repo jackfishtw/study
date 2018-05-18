9fee767c14688fef4d7b26e660bf8672103006eb
Jack Fish <jackfishtw@gmail.com>
2018-05-18 23:40:47 +0800

    建立第一版學習範例檔案

---
+++ hello.py
@@ -0,0 +1,11 @@
+#coding=utf-8
+
+# 這是測試
+"這是一個標準的格式"
+
+
+
+#程式本體
+if __name__ == "__main__":
+    import standdemo
+    print(standdemo.hello())
---
+++ standdemo.py
@@ -0,0 +1,15 @@
+# coding=utf-8
+# 這是測試
+"這是一個標準的格式"
+
+def hello():
+    """
+    這是多行說明
+
+
+    """
+    return("hello world")
+
+#程式本體
+if __name__ == "__main__":
+    print(hello())
